7135486b3020787f789a982150b095ddda9ef03d
jude90 <442119071@qq.com>
2015-11-19 17:50:37 +0800

    a random forest

--- src/main/scala/my/kaggle/Titanic.scala
+++ src/main/scala/my/kaggle/Titanic.scala
@@ -8,18 +8,24 @@ import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.tree.DecisionTree
+import org.apache.spark.mllib.tree.RandomForest
+
 import org.apache.spark.mllib.tree.model.DecisionTreeModel
+import org.apache.spark.mllib.tree.model.RandomForestModel
+
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.sql.types.{StructType, StructField, StringType, IntegerType};
-
-
+import org.apache.spark.sql.Row
+import org.apache.spark.mllib.linalg.Vectors
 case class Passenger(pid:Int,survived:Int,
                      pclass:Int,name:String,
                      Sex:String,Age:String,
                      sibsp:Int,parch:Int,
                      ticket:String, fare:Double,
                      cabin:String,embarked:String)
+
+case class Predcit(pid:Int,survived:Int)
 
 object Titanic {
     val sconf = new SparkConf().setAppName("Titanic-survive").setMaster("local[*]")
@@ -34,31 +40,111 @@ object Titanic {
           .option("inferSchema", "true")
           .option("header", "true")
           .load("train.csv")
-//        val df = sc.textFile("train.csv").map(_.split(","))
-//          .map{p =>
-//            Passenger(p(0).toInt, p(1).toInt,
-//                      p(2).toInt, p(3).trim,
-//                      p(4).trim, p(5).trim,
-//                      p(6).toInt, p(7).toInt,
-//                      p(8).trim, p(9).toDouble,
-//                      p(10).trim,p(11).trim)
-//
-//        }.toDF()
+        val tests = sqlContext.read
+          .format("com.databricks.spark.csv")
+          .option("inferSchema", "true")
+          .option("header", "true")
+          .load("test.csv")
+//      passenger.describe("Age").show()
+//      passenger.describe("Cabin").show()
+//      passenger
+//      passenger.groupBy($"Fare").count().show()
+//      passenger.groupBy($"Embarked").count().show()
+//      passenger.groupBy($"Survived",$"Pclass").count().show()
+//      passenger.groupBy($"Survived",$"Cabin").count().show()
+//      passenger.groupBy($"Survived",$"Embarked").count().show()
+//      passenger.groupBy($"Survived",$"Embarked").count().show()
+//      passenger.groupBy($"Survived",$"Sex").count().show()
+//  PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
+    val labels =   passenger.map{row :Row =>
 
-//        df.registerTempTable("titantic")
-//        passenger.show()
-      passenger.describe("Age").show()
-      passenger.describe("Cabin")
-     val freq =  passenger.stat.freqItems(Array("Cabin","Embarked"))
-      freq.collect().foreach{ row =>
-      println(row(0))
-        println(row(1))
+    val survived = row.getAs[Int]("Survived").toDouble
+
+    val pclass  = row.getAs[Int]("Pclass").toDouble
+    val age = row.getAs[Double]("Age")
+    val sibsp = row.getAs[Int]("SibSp").toDouble
+    val parch = row.getAs[Int]("Parch").toDouble
+    val fare = row.getAs[Double]("Fare")
+    val sex_cl:Double = row.getAs[String]("Sex") match {
+          case "male" => 0
+          case "female" =>1
+        }
+    val embarked_cl:Double = row.getAs[String]("Embarked") match {
+          case "Q" => 1
+          case "S" => 2
+          case "C" => 3
+          case _ => 0
+        }
+
+        LabeledPoint(survived.asInstanceOf[Int].toDouble,
+          Vectors.dense(Array(pclass,sex_cl ,age, sibsp, parch,fare))
+          )
+
       }
-        //        val fare_mean = raw_train.map { line =>
-//            line.split(",")
-//        }
 
-    }
+      val testfeature = tests.map{ row :Row =>
+
+//          val survived = row.getAs[Double]("Survived")
+        val pid = row.getAs[Int]("PassengerId")
+        val pclass  = row.getAs[Int]("Pclass").toDouble
+        val age = row.getAs[Double]("Age")
+        val sibsp = row.getAs[Int]("SibSp").toDouble
+        val parch = row.getAs[Int]("Parch").toDouble
+          val fare = row.getAs[Double]("Fare")
+          val sex_cl:Double = row.getAs[String]("Sex") match {
+            case "male" => 0
+            case "female" =>1
+          }
+          val embarked_cl:Double = row.getAs[String]("Embarked") match {
+            case "Q" => 1
+            case "S" => 2
+            case "C" => 3
+            case _ => 0
+          }
+        (pid, Vectors.dense(Array(pclass,sex_cl ,age, sibsp, parch,fare)))
+
+      }
+      // Train a DecisionTree model.
+      //  Empty categoricalFeaturesInfo indicates all features are continuous.
+      val numClasses = 2
+      val categoricalFeaturesInfo = Map[Int, Int]()
+      val impurity = "entropy"
+      val maxDepth = 10
+      val maxBins = 64
+      val featureSubsetStrategy = "auto"
+      val numTrees = 5
+      val treemodel = RandomForest.trainClassifier(labels,2,
+                      categoricalFeaturesInfo,numTrees,
+        featureSubsetStrategy,impurity,maxDepth, maxBins)
+      val labelpredicts = labels.map{ case LabeledPoint(label, features) =>
+        val predict = treemodel.predict(features)
+        (label, predict)
+      }
+
+      val trainErr = labelpredicts.filter{case(label, predict) =>
+        label != predict
+      }.count().toDouble / labelpredicts.count()
+      println("Test Error = " + trainErr)
+
+      val testpredicts = testfeature.map{ case(pid,feature) =>
+        (pid, treemodel.predict(feature).toInt)
+      }
+      testpredicts.coalesce(1).saveAsTextFile("predicts.csv")
+//      testpredicts.foreach(print)
+      println("Learned classification tree model:\n" + treemodel.toDebugString)
+
+
+
+      //      passenger.select("Embarked").distinct.foreach{ row:Row => println(row.getString(0))}
+      }
+
+
+
+
+
+
+
+
 
 //    def clean_data(line :String): LabeledPoint ={
 //        survival        Survival
