b3903e0fbf34acc530ceb4e11cdfb0973c445918
jude90 <442119071@qq.com>
2015-11-23 10:16:29 +0800

    search for best params

--- src/main/scala/my/kaggle/Titanic.scala
+++ src/main/scala/my/kaggle/Titanic.scala
@@ -4,11 +4,27 @@ package my.kaggle
  * Created by jude on 2015/11/15.
  */
 
+/**        survival        Survival
+**                        (0 = No; 1 = Yes)
+//        pclass          Passenger Class
+//                        (1 = 1st; 2 = 2nd; 3 = 3rd)
+//        name            Name
+//        sex             Sex
+//        age             Age
+//        sibsp           Number of Siblings/Spouses Aboard
+//        parch           Number of Parents/Children Aboard
+//        ticket          Ticket Number
+//        fare            Passenger Fare
+//        cabin           Cabin
+//          embarked        Port of Embarkation
+          (C = Cherbourg; Q = Queenstown; S = Southampton)
+*/
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.tree.DecisionTree
 import org.apache.spark.mllib.tree.RandomForest
+import org.apache.spark.mllib.tree.GradientBoostedTrees
 
 import org.apache.spark.mllib.tree.model.DecisionTreeModel
 import org.apache.spark.mllib.tree.model.RandomForestModel
@@ -34,20 +50,21 @@ object Titanic {
     import sqlContext.implicits._
 
     def main(args: Array[String]) {
-        val raw_train = sc.textFile("train.csv").cache()
         val passenger = sqlContext.read
           .format("com.databricks.spark.csv")
           .option("inferSchema", "true")
           .option("header", "true")
-          .load("train.csv")
+          .load("data/titanic/train.csv")
         val tests = sqlContext.read
           .format("com.databricks.spark.csv")
           .option("inferSchema", "true")
           .option("header", "true")
-          .load("test.csv")
+          .load("data/titanic/test.csv")
+
 //      passenger.describe("Age").show()
 //      passenger.describe("Cabin").show()
 //      passenger
+//      passenger.groupBy($"Age").count().show()
 //      passenger.groupBy($"Fare").count().show()
 //      passenger.groupBy($"Embarked").count().show()
 //      passenger.groupBy($"Survived",$"Pclass").count().show()
@@ -55,6 +72,8 @@ object Titanic {
 //      passenger.groupBy($"Survived",$"Embarked").count().show()
 //      passenger.groupBy($"Survived",$"Embarked").count().show()
 //      passenger.groupBy($"Survived",$"Sex").count().show()
+//      passenger.select("Fare").na.fill(passenger.describe("Fare").where($"summary" eqNullSafe "mean").take(0).getAs[Double])
+
 //  PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
     val labels =   passenger.map{row :Row =>
 
@@ -77,11 +96,11 @@ object Titanic {
         }
 
         LabeledPoint(survived.asInstanceOf[Int].toDouble,
-          Vectors.dense(Array(pclass,sex_cl ,fare))
+          Vectors.dense(Array(pclass,sex_cl,fare))
           )
 
       }
-
+    val Array(train, valid) = labels.randomSplit(Array(0.7,0.3))
       val testfeature = tests.map{ row :Row =>
 
 //          val survived = row.getAs[Double]("Survived")
@@ -101,44 +120,46 @@ object Titanic {
             case "C" => 3
             case _ => 0
           }
-        (pid, Vectors.dense(Array(pclass,sex_cl ,fare)))
+        (pid, Vectors.dense(Array(pclass,sex_cl,fare)))
 
       }
       // Train a DecisionTree model.
       //  Empty categoricalFeaturesInfo indicates all features are continuous.
-      val numClasses = 2
-      val categoricalFeaturesInfo = Map[Int, Int]()
-      val impurity = "entropy"
-      val maxDepth = 10
-      val maxBins = 64
-      val featureSubsetStrategy = "auto"
-      val numTrees = 5
-      val treemodel = RandomForest.trainClassifier(labels,2,
-                      categoricalFeaturesInfo,numTrees,
-        featureSubsetStrategy,impurity,maxDepth, maxBins)
-      val labelpredicts = labels.map{ case LabeledPoint(label, features) =>
-        val predict = treemodel.predict(features)
-        (label, predict)
+      for (depth <- Array(7,9,10,12,15); trees <- Array(2,4,7,9)){
+
+        val numClasses = 2
+        val categoricalFeaturesInfo = Map[Int, Int]()
+        val impurity = "entropy"
+        val maxDepth = 4
+        val maxBins = 32
+        val featureSubsetStrategy = "auto"
+        val numTrees = 3
+        val treemodel = RandomForest.trainClassifier(train,numClasses,
+          categoricalFeaturesInfo,trees,
+          featureSubsetStrategy,impurity,depth, maxBins)
+        val labelpredicts = valid.map{ case LabeledPoint(label, features) =>
+          val predict = treemodel.predict(features)
+          (label, predict)
+        }
+
+        val trainErr = labelpredicts.filter{case(label, predict) =>
+          label != predict
+        }.count().toDouble / labelpredicts.count()
+        println(s"predicts depth=${depth} trees=${trees} ,Test Error = ${trainErr}")
+
+        val testpredicts = testfeature.map{ case(pid,feature) =>
+          (pid, treemodel.predict(feature).toInt)
+        }
+
+        testpredicts.coalesce(1).map{case (id, p)=>
+          Array(id,p).mkString(",")
+        }.saveAsTextFile(s"predicts_depth=${depth}_trees=${trees}")
+
       }
 
-      val trainErr = labelpredicts.filter{case(label, predict) =>
-        label != predict
-      }.count().toDouble / labelpredicts.count()
-      println("Test Error = " + trainErr)
-
-      val testpredicts = testfeature.map{ case(pid,feature) =>
-        (pid, treemodel.predict(feature).toInt)
-      }
-
-      testpredicts.coalesce(1).map{case (id, p)=>
-      Array(id,p).mkString(",")
-      }.saveAsTextFile("predicts.csv")
 //      testpredicts.foreach(print)
-      println("Learned classification tree model:\n" + treemodel.toDebugString)
+//      println("Learned classification tree model:\n" + treemodel.toDebugString)
 
-
-
-      //      passenger.select("Embarked").distinct.foreach{ row:Row => println(row.getString(0))}
       }
 
 
@@ -149,23 +170,6 @@ object Titanic {
 
 
 
-//    def clean_data(line :String): LabeledPoint ={
-//        survival        Survival
-//                        (0 = No; 1 = Yes)
-//        pclass          Passenger Class
-//                        (1 = 1st; 2 = 2nd; 3 = 3rd)
-//        name            Name
-//        sex             Sex
-//        age             Age
-//        sibsp           Number of Siblings/Spouses Aboard
-//        parch           Number of Parents/Children Aboard
-//        ticket          Ticket Number
-//        fare            Passenger Fare
-//        cabin           Cabin
-//          embarked        Port of Embarkation
-//          (C = Cherbourg; Q = Queenstown; S = Southampton)
 
-//        val field :Seq[String] = line.split(",")
 
-//    }
 }
